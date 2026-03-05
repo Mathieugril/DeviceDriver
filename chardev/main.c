96b7eab2c6de76f2039939a546c7536d20c205fc
Matt <25468049@studentmail.ul.ie>
2026-03-05 15:16:29 +0000

    Altered makefile to put stuff in sub-folder + added placeholder fops functions and prototypes

--- chardev/main.c
+++ chardev/main.c
@@ -8,12 +8,12 @@ MODULE_DESCRIPTION("Message handling system");
 
 static int major;
 
-static ssize_t my_read(struct file *f, char __user *u, size_t l, loff_t *o)
-{
-	printk("main.c - read is called\n");
-	return 0;
+int my_open(struct inode *, struct file *);
+int my_release(struct inode *, struct file *);
+static ssize_t my_read(struct file *f, char __user *u, size_t l, loff_t *o);
+ssize_t my_write(struct file *, const char *, size_t, loff_t *);
+int my_ioctl(struct inode *, struct file *, unsigned int, unsigned long);
 
-}
 
 static struct file_operations fops = {
 	.read = my_read
@@ -36,5 +36,33 @@ static void __exit my_exit(void)
 	unregister_chrdev(major, "main.c");
 
 }
+
+int my_open(struct inode *, struct file *)
+{
+	printk("main.c - open is called\n");
+	return 0;
+}
+int my_release(struct inode *, struct file *)
+{
+	printk("main.c - release is called\n");
+	return 0;
+}
+static ssize_t my_read(struct file *f, char __user *u, size_t l, loff_t *o)
+{
+	printk("main.c - read is called\n");
+	return 0;
+}
+ssize_t my_write(struct file *, const char *, size_t, loff_t *)
+{
+	printk("main.c - write is called\n");
+	return 0;
+}
+int my_ioctl(struct inode *, struct file *, unsigned int, unsigned long)
+{
+	printk("main.c - ioctl is called\n");
+	return 0;
+}
+
+
 module_init(my_init);
 module_exit(my_exit);
