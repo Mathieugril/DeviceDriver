2095146a561ee60b0dba025e92f511f227997a07
Matt <25468049@studentmail.ul.ie>
2026-03-13 12:10:35 +0000

    added some comments to proc file

--- mb_proc.c
+++ mb_proc.c
@@ -1,11 +1,9 @@
 #include "mailbox.h"
 
-//  ssize_t procfs_read(struct file *filp, char __user *buffer, size_t length, loff_t *offset);
-//  ssize_t procfs_write(struct file *file, const char __user *buffer, size_t len, loff_t *off);
-//  int procfs_open(struct inode *inode, struct file *file);
-//  int procfs_close(struct inode *inode, struct file *file);
 
-void copy_string_to_buffer(char* string,unsigned long *postion){
+
+//does as says (string.h + strcat unavailable in kernel)
+void copy_string_to_buffer(char* string,unsigned long *postion){  
     for(int i=0;;i++){
         printk("%s  %lu   %d \n",string,*postion,i);
         if(string[i]=='\0'||*postion>PROCFS_MAX_SIZE){
@@ -17,7 +15,7 @@ void copy_string_to_buffer(char* string,unsigned long *postion){
     }
     return;
 }
-void copy_char_to_buffer(char character,unsigned long *postion){
+void copy_char_to_buffer(char character,unsigned long *postion){ 
     procfs_buffer[*postion]=character;
     *postion+=1;
     return;
@@ -25,12 +23,12 @@ void copy_char_to_buffer(char character,unsigned long *postion){
 
 // //void set_proc_buffer();
 
-void set_proc_buffer_contents(void){
-    unsigned long current_postition=0;
-    for(int i=0;i<PROCFS_MAX_SIZE;i++){
+void set_proc_buffer_contents(void){    //generates contents to put in proc file 
+    unsigned long current_postition=0;  //keeps track of postion in string. also erases anything already 
+    for(int i=0;i<PROCFS_MAX_SIZE;i++){ //initialise everything in strinng to prevent linux from thinking its a proc file
 		procfs_buffer[i]=' ';
 	}
-    for(int i=0;i<CHANNELS_NUM;i++){
+    for(int i=0;i<CHANNELS_NUM;i++){    //actual contents start
         mb_channel_s channel = mb_build.channels[i];
         printk("postition; %lu",current_postition);
         copy_string_to_buffer("channel \0",&current_postition);
@@ -44,45 +42,47 @@ void set_proc_buffer_contents(void){
         copy_string_to_buffer("\n   text: \0",&current_postition);
         copy_string_to_buffer(channel.messages->text,&current_postition);
         copy_string_to_buffer("\n \0",&current_postition);
-    }
+    } //actual contents end
 
     procfs_buffer_size=current_postition;
 
     return;
 }
 
+//called every time the proc file is opened
  ssize_t procfs_read(struct file *filp, char __user *buffer, size_t length, loff_t *offset)  
 {   
     
 
 	
-    set_proc_buffer_contents();
+    set_proc_buffer_contents(); //does as says
 
 
-    if (*offset || procfs_buffer_size+1 == 0) {  
-        pr_debug("procfs_read: ENDer yes\n ");
+    if (*offset || procfs_buffer_size == 0) { //prints when buffer is empty  
+        pr_debug("procfs_read: END\n ");
  
         *offset = 0;  
         return 0;  
     }  
-    procfs_buffer_size = min(procfs_buffer_size, length);
-    printk("%s %lu",procfs_buffer,procfs_buffer_size);
-    if (copy_to_user(buffer, procfs_buffer, procfs_buffer_size))
+    procfs_buffer_size = min(procfs_buffer_size, length);   //makes sure the read size is not exceeded?
+    //printk("%s %lu",procfs_buffer,procfs_buffer_size);
+    if (copy_to_user(buffer, procfs_buffer, procfs_buffer_size))//puts proc buffer contents in proc file
  
         return -EFAULT;  
-    *offset += procfs_buffer_size;  
+    *offset += procfs_buffer_size;  //i dont actually kn what the buffer size is.
 
  
     pr_debug("procfs_read: read %lu bytes\n", procfs_buffer_size);  
-    return procfs_buffer_size;
+    return procfs_buffer_size; 
  
 }  
 
-ssize_t procfs_write(struct file *file, const char __user *buffer, size_t len, loff_t *off)  
+//not really used, can change proc buffers contents, but thats redone every read call. probably called when proc file
+ssize_t procfs_write(struct file *file, const char __user *buffer, size_t len, loff_t *off)   
 {  
     procfs_buffer_size = min(PROCFS_MAX_SIZE, len);
  
-    if (copy_from_user(procfs_buffer, buffer, procfs_buffer_size))  
+    if (copy_from_user(procfs_buffer, buffer, procfs_buffer_size))//sets buffer contents to contents of proc file  
         return -EFAULT;
  
     *off += procfs_buffer_size;  
